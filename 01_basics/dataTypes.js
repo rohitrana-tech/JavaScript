f914be2c3a968fae5582877f8dffab8d47d7e1fa
Rohit Rana <rohitrana4354@gmail.com>
2025-11-07 15:57:09 +0000

    DataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; 
+/* treat all JS code as newer version . 
+ But in current scenario we don't need to use "use strict".
+Because all the things run in strict form by default. */
+
+ //alert (3+3)
+ // This syntax works earlier when js run from browser but now js run using nodejs so this syntax not workes anymore.
+
+ console.log(3+3)
+ // code readability should be high.
+
+ console.log("Rohit")
+
+
+ let name = "Rohit Rana"  // String
+ let age = 21             // Number or Integer 
+ let LoggedIn = false     // Boolean
+
+ // number => range (2 power 53 - 1).
+ // BigInt => Use for very large number no upper and lower limit available.
+ // String => " "
+ // Boolean => For True or False value .
+ // null => Standalone Value and dataType as well.
+ // undefined => No value assign yet but variable declaration is complete.
+ // symbol => use to define uniqueness.
+
+ // object 
+
+ console.log(typeof undefined) // undefined
+ console.log(typeof null) // Object
+ 
+ 
