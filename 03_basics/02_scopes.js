cdf8d9a1e37f3c199bf38dde4bb9fed3f497e395
Rohit Rana <rohitrana4354@gmail.com>
2025-11-25 16:00:16 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+let a = 300
+
+if(true){
+let a = 10
+const b = 20
+var c = 30
+console.log("Inner ", a);
+
+}
+ /* Here var ki value if statement ke andar ha fir
+bhi run ho rahi hai jabki a and b run nahi ho rahi
+ wo error message show kar rahi hai a or b is not defined. */
+
+console.log(a);
+//console.log(b);
+//console.log(c);
